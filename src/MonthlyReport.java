57e93037c740e40e129405f6724e2c41dc394638
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-05-11 17:08:01 +0300

    Feat: добавить FileReader

---
+++ src/MonthlyReport.java
@@ -0,0 +1,6 @@
+public class MonthlyReport {
+
+    public MonthlyReport(String path) {
+
+    }
+}
---
+++ src/YearlyReport.java
@@ -0,0 +1,5 @@
+public class YearlyReport {
+    public YearlyReport(String report) {
+
+    }
+}
