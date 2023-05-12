edbaeafe7e98a9d6851a576322eb829885889f26
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-05-12 21:25:55 +0300

    Feat: добавить mainMenu

--- src/FileReader.java
+++ src/FileReader.java
@@ -4,14 +4,16 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class FileReader {
-    ArrayList<String> readFileContents(String fileName) {
-        String path = "./resources/" + fileName;
-        try {
-            return new ArrayList<>(Files.readAllLines(Path.of(path)));
-        } catch (IOException e) {
-            System.out.println("Невозможно прочитать файл с отчётом. Возможно, файл отсутствует в нужной директории.");
-            return new ArrayList<>();
-        }
-    }
 
-} 
+  ArrayList<String> readFileContents(String fileName) {
+    String path = "./resources/" + fileName;
+    try {
+      return new ArrayList<>(Files.readAllLines(Path.of(path)));
+    } catch (IOException e) {
+      System.out.println(
+        "Невозможно прочитать файл с отчётом. Возможно, файл отсутствует в нужной директории."
+      );
+      return new ArrayList<>();
+    }
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // Поехали!
-    }
-}
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+    FileReader fileReader = new FileReader();
+    ArrayList contentYearFile = fileReader.readFileContents("y.2021.csv");
+    System.out.println(contentYearFile);
+
+    while (true) {
+      mainMenu();
+
+      String command = scanner.nextLine();
+      if (command.equals("1")) {
+        System.out.println(1);
+      } else if (command.equals("2")) {
+        System.out.println(2);
+      } else if (command.equals("3")) {
+        System.out.println(3);
+      } else if (command.equals("4")) {
+        System.out.println(4);
+      } else if (command.equals("5")) {
+          System.out.println(5);
+      } else if (command.equals("Exit")) {
+        return;
+      } else {
+        System.out.println("Кажется такой команды нет...;(");
+      }
+    }
+  }
+
+  public static void mainMenu() {
+    System.out.println("Введите команду:");
+    System.out.println("1: Считать все месячные отчёты");
+    System.out.println("2: Считать годовой отчёт");
+    System.out.println("3: Сверить отчёты");
+    System.out.println("4: Вывести информацию обо всех месячных отчётах ");
+    System.out.println("5: Вывести информацию о годовом отчёте");
+    System.out.println("Exit: Выход");
+  }
+}
